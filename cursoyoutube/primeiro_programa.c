9491699a4361a9b8791a1a1f72bbd87c6f479a9b
macesilveira <bahiacematheus@gmail.com>
2025-04-28 22:21:00 -0300

    curso yt

--- cursoyoutube/primeiro_programa.c
+++ cursoyoutube/primeiro_programa.c
@@ -2,7 +2,7 @@
 
 int main (){
     
-    printf (" Meu primeiro programa no curso do Youtube.\n"); //barra ao contrário n pula uma linha
+    printf ("Meu primeiro programa no curso do Youtube.\n"); //barra ao contrário n pula uma linha
 
     return 0;
     // comentário em uma linha 
