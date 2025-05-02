beaf4083c87ce117d72d21026a22b1a4639e3719
macesilveira <bahiacematheus@gmail.com>
2025-05-01 21:43:28 -0300

    jogo de adivinhação - curso alura

---
+++ cursoalura/adivinhação.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    // Imprime o cabeçalho do jogo
+
+    printf("*********************\n");
+    printf(" Jogo de adivinhacao \n");
+    printf("*********************\n");
+
+    int numero_secreto=42;
+
+    printf("%d", numero_secreto);
+}
