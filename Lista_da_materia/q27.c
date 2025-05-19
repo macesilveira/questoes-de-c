143829ee99064775045676ae4987dda127f65e95
Macé Silveira <104800722+macesilveira@users.noreply.github.com>
2025-05-19 18:28:55 -0300

    Create q27.c
    lista para prova de logica

---
+++ Lista_da_materia/q27.c
@@ -0,0 +1,15 @@
+// Escreva um programa que leia 3 números e calcule a média ponderada entre eles. Considere que o maior número recebe peso 5 e os outros dois recebem peso 2,5.
+
+#include <stdio.h>
+
+int main(){
+	
+	float num1, num2, num3;
+	
+	printf("Escreva 3 numeros: ");
+	scanf("%f %f %f",&num1, &num2, &num3);
+	
+
+	
+	return 0;
+}
