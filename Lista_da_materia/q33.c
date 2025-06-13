961a9a221effe50bf64b6a98a25487a66e02a927
macesilveira <bahiacematheus@gmail.com>
2025-06-12 23:57:05 -0300

    lista da materia

--- Lista_da_materia/q33.c
+++ Lista_da_materia/q33.c
@@ -2,3 +2,34 @@
 de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
 ponto, ou se está sobre um dos eixos cartesianos ou na origem (x=y=0). */
 
+#include <stdio.h>
+
+int main(){
+
+    int x, y;
+
+    printf("Digite o valor de X: ");
+    scanf("%d",&x);
+    printf("Digite o valor de Y: ");
+    scanf("%d", &y);
+
+    if (x==0 && y==0){
+        printf("O ponto esta na origem\n");
+    } else if (y!=0 && x==0){
+        printf("O ponto esta sobre o eixo Y\n");
+    } else if (x!=0 && y==0){
+        printf("O ponto esta sobre o eixo X\n");
+    } else if (x>0 && y>0){
+        printf("O ponto esta no primeiro quadrante\n");
+    } else if (x<0 && y>0){
+        printf("O ponto esta no segundo quadrante\n");
+    } else if (x<0 && y<0){
+        printf("O ponto esta no terceiro quadrante\n");
+    }else{
+        printf("O ponto esta no quarto quadrante\n");
+    }
+
+
+
+    return 0;
+}
